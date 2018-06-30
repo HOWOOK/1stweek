65459b073f90cad36b4158cef9889997866fa023
HOWOOK <hwshin1995@naver.com>
2018-07-01 02:17:34 +0900

    설명 주석 추가

--- app/src/main/java/com/example/q/practice3/Tab1Contacts.java
+++ app/src/main/java/com/example/q/practice3/Tab1Contacts.java
@@ -9,6 +9,11 @@ import android.widget.TextView;
 
 public class Tab1Contacts extends Fragment{
 
+
+    //Fragment가 UI를 처음으로 그리고자할 때에 호출된다.
+    //Fragment를 통해 UI를 그리고자 한다면 이 함수의 결과로 Fragment layout의 루트에 해당하는 View를 리턴해야한다.
+    //만약 null을 리턴한다면 Fragment는 UI를 제공하지 않는다.
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
