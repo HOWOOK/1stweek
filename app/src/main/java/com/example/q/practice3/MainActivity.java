7c3fb7e3e2f21e52ba373b1e870198d457668574
HOWOOK <hwshin1995@naver.com>
2018-06-30 14:24:50 +0900

    처음

--- app/src/main/java/com/example/q/practice3/MainActivity.java
+++ app/src/main/java/com/example/q/practice3/MainActivity.java
@@ -90,40 +90,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
+    //deleted PlaceholderFragment class from here
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -137,9 +104,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+           switch (position){
+               case 0:
+                   Tab1Contacts tab1 = new Tab1Contacts();
+                   return tab1;
+               case 1:
+                   Tab2Pictures tab2 = new Tab2Pictures();
+                   return tab2;
+               case 2:
+                   Tab3Anon tab3 = new Tab3Anon();
+                   return tab3;
+
+               default:
+                   return null;
+           }
         }
 
         @Override
@@ -147,5 +125,19 @@ public class MainActivity extends AppCompatActivity {
             // Show 3 total pages.
             return 3;
         }
+
+//        @Override
+//        public CharSequence getPageTitle (int position){
+//            switch (position) {
+//                case 0:
+//                    return "CONTACTS";
+//                case 1:
+//                    return "PICTURES";
+//                case 2:
+//                    return "ANON";
+//            }
+//            return null;
+//
+//            }
+        }
     }
-}
